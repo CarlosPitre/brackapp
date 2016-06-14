942fee876d2534605f3885e65e7fbef1a5911817
luis-serge <luis-serge@hotmail.com>
2016-06-14 11:35:46 -0500

    nada

--- empresa/js/scripts/controllers/miperfilCtrl.js
+++ empresa/js/scripts/controllers/miperfilCtrl.js
@@ -1,12 +1,25 @@
 app.controller('miperfilCtrl', function($scope,miperfilService,$routeParams,pluginsService){
 	
 
+
+   
+
 	$scope.Profesionales = [];
 	$scope.Profesional = {};
 	$scope.idProfesional = "1" //localstorage.getItem("idPro")
 
 
 	loadDatos();
+
+
+
+
+
+
+	$scope.hoveringOver = function(value) {
+		$scope.overStar = value;
+		$scope.percent = 100 * (value / $scope.max);
+	};
 
 
 	function loadDatos () {
